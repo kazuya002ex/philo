7631a22a2874861fff936f9880556a3cd413842a
holasoynaoki <naoki13futsal@gmail.com>
2021-11-26 22:56:44 +0900

    Create simple modal (#2)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,30 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("DOMContentLoaded", function(){
+  const buttonOpen = document.getElementById('modalOpen');
+  const modal = document.getElementById('easyModal');
+  const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+  //ボタンがクリックされた時
+  buttonOpen.addEventListener('click', modalOpen);
+  function modalOpen() {
+    console.log("aaa");
+    modal.style.display = 'block';
+  };
+
+  //戻るがクリックされた時
+  buttonClose.addEventListener('click', modalClose);
+  function modalClose() {
+    modal.style.display = 'none';
+  };
+
+  //モーダルコンテンツ以外がクリックされた時
+  addEventListener('click', outsideClose);
+  function outsideClose(e) {
+    if (e.target == modal) {
+      modal.style.display = 'none';
+    };
+  };
+}, false);
