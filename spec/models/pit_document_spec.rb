5a70085b7dfc21b1d2ceed855d0d0517d2dc4647
kazuya002ex <chiharu.event2@gmail.com>
2021-12-04 10:27:49 +0900

    feat: メッセージの日本語化

--- spec/models/pit_document_spec.rb
+++ spec/models/pit_document_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PitDocument, type: :model do
     pit_document.valid?
 
     it 'インスタンスの生成が無効になる' do
-        expect(pit_document.errors.full_messages).to include('Imageを入力してください')
+        expect(pit_document.errors.full_messages).to include('画像またはPDFを入力してください')
         expect(pit_document).to_not be_valid
     end
   end
