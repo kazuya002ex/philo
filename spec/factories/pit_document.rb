18f7ca1ab24aea8e121fa592f6eb591b7602c8c9
kazuya002ex <chiharu.event2@gmail.com>
2021-12-12 23:45:45 +0900

    spec: imageのところをpdfに変えた

--- spec/factories/pit_document.rb
+++ spec/factories/pit_document.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :pit_document do
     title { "無題のドキュメント" }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+    pdf { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
     uuid { "39f03a27-6fcd-40a9-935b-8a0fff7e0769" }
   end
 end
--- spec/requests/pit_documents_spec.rb
+++ spec/requests/pit_documents_spec.rb
@@ -5,10 +5,7 @@ RSpec.describe "PitDocuments", type: :request do
     let(:pit_document) { FactoryBot.create(:pit_document) }
 
     it '資料確認画面（仮）の表示に成功すること' do
-      pp pit_document
-      pp pit_document
-
-      get confirm_path, params: { pit_document: pit_document }
+      get new_pit_document_path, params: { pit_document: pit_document }
       expect(response).to have_http_status(200)
     end
   end
