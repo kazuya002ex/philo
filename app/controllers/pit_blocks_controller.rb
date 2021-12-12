294eeb7d7e4468fcf84873214394007a46a9975c
kazuya002ex <chiharu.event2@gmail.com>
2021-12-12 20:05:16 +0900

    feat: pit_blockにprivateメソッドを追加した

--- app/controllers/pit_blocks_controller.rb
+++ app/controllers/pit_blocks_controller.rb
@@ -2,4 +2,10 @@ class PitBlocksController < ApplicationController
 
   def create
   end
+
+  private
+
+  def pit_block_params
+    params.require(:pit_block).permit(:width, :height, :side_coordinate, :vertical_coordinate)
+  end
 end
